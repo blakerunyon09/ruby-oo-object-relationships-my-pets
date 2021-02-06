da5414acc52dfafe7e92cc46c065cc19c9dd296c
Blake Runyon <blakerunyon09@gmail.com>
2021-02-05 19:13:47 -0700

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,64 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+  
+  def initialize name
+    @name = name
+    @species = "human"
+
+    @@all << self
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def cats
+    Cat.all.filter { |el| el.owner == self }
+  end
+
+  def dogs
+    Dog.all.filter { |el| el.owner == self }
+  end
+
+  def buy_cat cat_name
+    cat_name = Cat.new(cat_name, self)
+  end
+
+  def buy_dog dog_name
+    dog_name = Dog.new(dog_name, self)
+  end
+
+  def walk_dogs
+    dogs.map { |el| el.mood = "happy" }
+  end
+
+  def feed_cats
+    cats.map { |el| el.mood = "happy" }
+  end
+
+  def sell_pets
+    cats.map { |el| el.mood = "nervous" }
+    dogs.map { |el| el.mood = "nervous" }
+    cats.map { |el| el.owner = nil }
+    dogs.map { |el| el.owner = nil }
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
 end
---
+++ lib/test-data.rb
@@ -0,0 +1,20 @@
+# require 'pry'
+# require_relative './dog'
+# require_relative './cat'
+# require_relative './owner'
+
+# owner1 = Owner.new("Bob")
+# owner2 = Owner.new("Kilt")
+# owner3 = Owner.new("Jeremy")
+
+# cat1 = Cat.new("Bunger", owner1)
+# cat2 = Cat.new("Snickers", owner1)
+# cat3 = Cat.new("Tom", owner1)
+# cat4 = Cat.new("Monkey", owner2)
+
+# dog1 = Dog.new("Peanut", owner2)
+# dog2 = Dog.new("Peace", owner2)
+# dog3 = Dog.new("Molly", owner3)
+
+# binding.pry
+0
